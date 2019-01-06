ad3dea0f65ae6f64cd01a09a9400fa3778a4fef7
alexandergibson <44134171+alexandergibson@users.noreply.github.com>
2019-01-06 04:23:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-     self.redirect("https://jbirdie.com/ads.php?key=why-rosegal", True)
+     self.redirect("http://affiliateads.go2cloud.org/aff_c?offer_id=3335&aff_id=2403", True)
 		# and this is the main code
 
 app = webapp2.WSGIApplication([
